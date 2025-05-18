9101395b9c621e3982ef35d673f9c0eb930ec6d1
Fer Rodríguez <ferrodriguez2509@gmail.com>
2025-05-18 13:31:30 -0600

    feat:version funcional para un sistema constructivo y una capa

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import plotly.express as px
 import enerhabitat as eh
-from shiny import App, ui, render
+import os
+
+from shiny import App, ui, render, reactive
 from shinywidgets import output_widget, render_widget  
-from utils.card import panel_card
+
+from utils.card import panel_card,PRECARGADOS_DIR
 
 app_ui = ui.page_sidebar(
     ui.sidebar(
@@ -27,44 +30,67 @@ app_ui = ui.page_sidebar(
 )
 
 
-
 def server(input, output, session):
+    current_file = reactive.Value(None)
+    
+    @reactive.Calc
+    def solucion():
+        sol_data = eh.Tsa(
+            solar_absortance = float(input.absortancia()),
+            surface_tilt = float(input.tilt()), 
+            surface_azimuth= float(input.azimuth()),
+            month=input.mes(),
+            epw_file= current_file()
+            )
+        
+        sc = sistemaConstructivo()
+        
+        sol_data = eh.solveCS(sc, sol_data)
+        
+        return sol_data
+    
+    @reactive.Calc
+    def sistemaConstructivo():
+        return [(input.ancho_capa_1(), input.material_capa_1())]
+        
+        
+    @reactive.Effect
+    def _():
+        # Manejar selección de archivo precargado
+        if input.selector_archivo().startswith("precargado_"):
+            file_name = input.selector_archivo().replace("precargado_", "", 1)
+            file_path = os.path.join(PRECARGADOS_DIR, file_name)
+            current_file.set(file_path)
+    
+    @reactive.Effect
+    def _():
+        # Manejar archivo subido
+        if input.selector_archivo() == "upload" and input.epw_file() is not None:
+            file_info = input.epw_file()[0]
+            current_file.set(file_info["datapath"])
+            
+    @output
+    @render.ui
+    def ui_upload():
+        if input.selector_archivo() == "upload":
+            return ui.input_file("epw_file",label='', accept=[".epw"], multiple=False)
+        return None
+    
     @render.data_frame  
     def sol_df():
-        sol_data = eh.Tsa(
-        solar_absortance=0.8,
-        surface_tilt=input.tilt(), 
-        surface_azimuth= input.azimuth(),
-        month=input.mes(),
-        epw_file=input.epw_file()[0]['datapath']
-        )
+        sol_data = solucion()
         return render.DataGrid(sol_data)
     
     @render_widget
     def sol_plot():
-        sol_data = eh.Tsa(
-        solar_absortance=0.8,
-        surface_tilt=input.tilt(), 
-        surface_azimuth= input.azimuth(),
-        month=input.mes(),
-        epw_file=input.epw_file()[0]['datapath']
-        )
+        sol_data = solucion()
         
-        sc = [
-            (0.001,"steel"),
-#           (0.1, "adobe")
-#           (0.02, "brick"),
-#           (0.1, "concrete"),
-        ]
-        
-        eh.solveCS(sc, sol_data)
-        
-        solucion=px.scatter(
+        solucion_plot = px.scatter(
             data_frame=sol_data,
             x=sol_data.index,
             y=["Ta","Tsa","Ti"]
         )
-        return solucion
+        return solucion_plot
 
 
 app = App(app_ui, server)
--- utils/card.py
+++ utils/card.py
@@ -1,5 +1,8 @@
 from shiny import ui
 import enerhabitat as eh
+import os
+
+PRECARGADOS_DIR="./epw/"
 
 meses = {
     "01": "Enero",
@@ -36,22 +39,26 @@ def panel_card(num):
     return ui.nav_panel(
         f"SC {num}",
         ui.h4("Datos climáticos"),
-        ui.input_file("epw_file", "Archivo EPW", accept=[".epw"], multiple=False),
-        ui.input_select("mes", "Mes:", meses, selected="Enero"),
+        ui.input_select(
+                id="selector_archivo",
+                label="Archivo EPW:",
+                choices={
+                    **{f"precargado_{archivo}": archivo 
+                        for i, archivo in enumerate(os.listdir(PRECARGADOS_DIR), 1) 
+                            if os.path.isfile(os.path.join(PRECARGADOS_DIR, archivo))},
+                    **{"upload": "↪ Subir mi propio archivo"}
+                }
+            ),
+        ui.output_ui("ui_upload"),
+        ui.input_select("mes", "Mes:", meses, selected="Enero",),
         ui.hr(),
         ui.h4("Parámetros geométricos"),
         ui.input_select("tilt", "Inclinación:", tilt),
         ui.input_select("azimuth", "Orientación:", azimuth),
         ui.hr(),
         ui.h4("Sistema constructivo"),
+        ui.input_select('material_capa_1', "Material:",materiales),
+        ui.input_numeric("ancho_capa_1", "Ancho (mm):",value=0.01,step=0.01,min=0.01),
+        ui.input_numeric("absortancia", "Absortancia:",value=0.8,min=0, max=1)
     )
 
-
-"""        
-        
-        
-        
-        
-        
-        
-        """
