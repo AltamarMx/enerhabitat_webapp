499d8986c3925ffe9d0be71f84c407aeb4a4fcca
Fer Rodríguez <ferrodriguez2509@gmail.com>
2025-05-26 04:28:01 -0600

    feat:ui

--- app.py
+++ app.py
@@ -3,60 +3,52 @@ import enerhabitat as eh
 import os
 
 from shiny import App, ui, render, reactive
-from shinywidgets import output_widget, render_widget  
+from shinywidgets import output_widget, render_widget
 
-from utils.card import panel_card,PRECARGADOS_DIR,materiales
+from utils.card import side_card, PRECARGADOS_DIR, materiales
 
-app_ui = ui.page_sidebar(
-    ui.sidebar(
+app_ui = ui.page_fluid(
+    ui.page_sidebar(
+        side_card(),
         ui.navset_card_tab(
-            panel_card(1),
-            id="cardtab_sc",           
+            ui.nav_panel(
+                "Gráfica",
+                output_widget("sol_plot"),
+            ),
+            ui.nav_panel("DataFrame", ui.output_data_frame("sol_df")),
         ),
-        id="sidebar_sc",
-        width=350,
-        position="right"
-    ),
-    ui.navset_card_tab(
-        ui.nav_panel(
-            "Gráfica",
-            output_widget("sol_plot"),
-        ),
-        ui.nav_panel(
-            "DataFrame",
-            ui.output_data_frame("sol_df")
-        )
     )
 )
 
+
 def server(input, output, session):
     current_file = reactive.Value(None)
-    
+
     # Lista para mantener el registro de capas existentes
     capas_activas = reactive.Value([1])  # Comienza con la capa 1
-    
+
     @reactive.Calc
     def solucion():
         sol_data = eh.Tsa(
-            solar_absortance = float(input.absortancia()),
-            surface_tilt = float(input.tilt()), 
-            surface_azimuth= float(input.azimuth()),
+            solar_absortance=float(input.absortancia()),
+            surface_tilt=float(input.tilt()),
+            surface_azimuth=float(input.azimuth()),
             month=input.mes(),
-            epw_file= current_file()
-            )
-        
+            epw_file=current_file(),
+        )
+
         sc = sistemaConstructivo()
         sol_data = eh.solveCS(sc, sol_data)
         return sol_data
-    
+
     @reactive.Calc
     def sistemaConstructivo():
         return [
             (input[f"ancho_capa_{i}"](), input[f"material_capa_{i}"]())
             for i in capas_activas()
-        ]    
-    
-    # Manejo de EPW's    
+        ]
+
+    # Manejo de EPW's
     @reactive.Effect
     def _():
         # Manejar selección de archivo precargado
@@ -64,21 +56,21 @@ def server(input, output, session):
             file_name = input.selector_archivo().replace("precargado_", "", 1)
             file_path = os.path.join(PRECARGADOS_DIR, file_name)
             current_file.set(file_path)
-    
+
     @reactive.Effect
     def _():
         # Manejar archivo subido
         if input.selector_archivo() == "upload" and input.epw_file() is not None:
             file_info = input.epw_file()[0]
             current_file.set(file_info["datapath"])
-    
-    # Agregar capa        
+
+    # Agregar capa
     @reactive.Effect
     @reactive.event(input.add_capa)
     def _():
         nueva_capa = max(capas_activas()) + 1
         capas_activas.set(capas_activas() + [nueva_capa])
-    
+
     # Eliminar capa
     @reactive.Effect
     def _():
@@ -90,15 +82,15 @@ def server(input, output, session):
                     nuevas_capas = [c for c in capas_activas() if c != i]
                     capas_activas.set(nuevas_capas)
                 break
-           
+
     @output
     @render.ui
     def ui_upload():
         # ui para subir archivo
         if input.selector_archivo() == "upload":
-            return ui.input_file("epw_file",label='', accept=[".epw"], multiple=False)
+            return ui.input_file("epw_file", label="", accept=[".epw"], multiple=False)
         return None
-    
+
     @output
     @render.ui
     def ui_capas():
@@ -107,37 +99,37 @@ def server(input, output, session):
             panels.append(
                 ui.accordion_panel(
                     f"Capa {i}",
-                    ui.input_select(f'material_capa_{i}', "Material:", materiales),
-                    ui.input_numeric(f"ancho_capa_{i}", "Ancho (mm):", value=0.1, step=0.01, min=0.01),
-                    ui.input_numeric("absortancia", "Absortancia:",value=0.8,min=0, max=1) if i==1 else None,
-                    ui.input_action_button(f"remove_capa_{i}", "Eliminar capa", class_="btn-light",width="100%") if len(capas_activas()) > 1 and i!=1 else None,
-                    ui.input_action_button("add_capa", "Agregar capa", class_="btn-primary",width="100%") if i == len(capas_activas()) else None,
+                    ui.input_select(f"material_capa_{i}", "Material:", materiales),
+                    ui.input_numeric(f"ancho_capa_{i}", "Ancho (mm):", value=0.1, step=0.01, min=0.01),                  
+                    ui.input_action_button(
+                        f"remove_capa_{i}",
+                        "Eliminar",
+                        width="100%",
+                        class_="btn-light") if len(capas_activas()) > 1 else None
+                    ),
                 )
-            )
-        
+            
+
         return ui.accordion(
             *panels,
             id="capas_accordion",
             open=f"Capa {max(capas_activas()) if capas_activas() else '1'}",  # Abre la última capa
-            multiple=False
+            multiple=False,
         )
-        
-    @render.data_frame  
+
+    @render.data_frame
     def sol_df():
         sol_data = solucion()
         return render.DataGrid(sol_data)
-    
+
     @render_widget
     def sol_plot():
         sol_data = solucion()
-        
+
         solucion_plot = px.scatter(
-            data_frame=sol_data,
-            x=sol_data.index,
-            y=["Ta","Tsa","Ti"]
+            data_frame=sol_data, x=sol_data.index, y=["Ta", "Tsa", "Ti"]
         )
         return solucion_plot
 
+
 app = App(app_ui, server)
-
-
--- utils/card.py
+++ utils/card.py
@@ -2,7 +2,7 @@ from shiny import ui
 import enerhabitat as eh
 import os
 
-PRECARGADOS_DIR="./epw/"
+PRECARGADOS_DIR = "./epw/"
 
 meses = {
     "01": "Enero",
@@ -22,42 +22,75 @@ meses = {
 tilt = {"90": "Muro", "0": "Techo"}
 
 azimuth = {
-    "0":"Norte",
-    "45":"Noroeste",
-    "90":"Oeste",
-    "135":"Suroeste",
-    "180":"Sur",
-    "225":"Sureste",
-    "270":"Este",
-    "315":"Noreste",
+    "0": "Norte",
+    "45": "Noreste",
+    "90": "Este",
+    "135": "Sureste",
+    "180": "Sur",
+    "225": "Suroeste",
+    "270": "Oeste",
+    "315": "Noroeste",
 }
 
 materiales = eh.get_list_materials()
 
 
-def panel_card(num):
-    return ui.nav_panel(
-        f"SC {num}",
-        ui.h4("Datos climáticos"),
-        ui.input_select(
+def side_card(titulo=None):
+    return ui.sidebar(
+        ui.card_header(f"{titulo}") if titulo else None,
+        # ui.input_dark_mode(),
+        ui.card(
+            ui.card_header("Datos cimáticos"),
+            ui.input_select(
                 id="selector_archivo",
                 label="Archivo EPW:",
                 choices={
-                    **{f"precargado_{archivo}": archivo 
-                        for i, archivo in enumerate(os.listdir(PRECARGADOS_DIR), 1) 
-                            if os.path.isfile(os.path.join(PRECARGADOS_DIR, archivo))},
-                    **{"upload": "↪ Subir mi propio archivo"}
-                }
+                    **{
+                        f"precargado_{archivo}": archivo
+                        for i, archivo in enumerate(os.listdir(PRECARGADOS_DIR), 1)
+                        if os.path.isfile(os.path.join(PRECARGADOS_DIR, archivo))
+                    },
+                    **{"upload": "↪ Subir mi propio archivo"},
+                },
             ),
-        ui.output_ui("ui_upload"),
-        ui.input_select("mes", "Mes:", meses, selected="Enero",),
-        ui.hr(),
-        ui.h4("Datos geométricos"),
-        ui.input_select("tilt", "Inclinación:", tilt),
-        ui.input_select("azimuth", "Orientación:", azimuth),
-        ui.hr(),
-        ui.h4("Sistema constructivo"),
-        ui.output_ui("ui_capas"),
-               
+            ui.output_ui("ui_upload"),
+            ui.input_select(
+                "mes",
+                "Mes:",
+                meses,
+                selected="Enero",
+            ),
+        ),
+        ui.card(
+            ui.card_header("Datos geométricos"),
+            ui.input_select("tilt", "Inclinación:", tilt),
+            ui.input_select("azimuth", "Orientación:", azimuth),
+        ),
+        ui.card(
+            ui.card_header("Sistemas constructivos"),
+            ui.navset_card_tab(
+                ui.nav_panel(
+                    "SC 1",
+                    ui.input_numeric(
+                        "absortancia", "Absortancia:", value=0.8, min=0, max=1
+                    ),
+                    ui.h5("Capas:"),
+                    ui.output_ui("ui_capas"),
+                    ui.input_action_button(
+                        "add_capa",
+                        "Agregar",
+                        width="100%",
+                        class_="btn-primary")
+                ),
+                ui.nav_panel("SC n"),
+                ui.nav_panel("SC n"),
+                ui.nav_panel("SC n"),
+            ),
+            ui.input_task_button(
+                "resolver_sc", "Calcular", label_busy="Calculando...", width="100%"
+            ),
+        ),
+        id="sidebar",
+        width=350,
+        position="right",
     )
-
