4a6a19067d4cf42caeedeecc811f69f79762d609
Fer Rodríguez <ferrodriguez2509@gmail.com>
2025-05-31 02:17:03 -0600

    feat:graficas Irr

--- app.py
+++ app.py
@@ -13,15 +13,22 @@ app_ui = ui.page_fluid(
         side_card(),
         ui.navset_card_tab(
             ui.nav_panel(
-                "Gráfica",
-                output_widget("sol_plot"),
-            ),
-            ui.nav_panel(
-                "Día promedio",
-                ui.output_data_frame("dia_df"),                          
-            ),
-            ui.nav_panel(
                 "Resultados",
+                ui.card(
+                    ui.card_header("Temperatura"),
+                    output_widget("sol_plot")
+                ),
+                ui.card(
+                    ui.card_header("Irradiancia"),
+                    output_widget("irr_plot")
+                )
+            ),
+            ui.nav_panel(
+                "Datos día promedio",
+                ui.output_data_frame("dia_df")                    
+            ),
+            ui.nav_panel(
+                "Datos resultados",
                 ui.output_data_frame("sol_df")
             )
         ),
@@ -63,9 +70,11 @@ def server(input, output, session):
     @reactive.event(input.resolver_sc)
     def calculate_solucion():
         # Solo se ejecuta cuando se presiona el botón resolver_sc
-        sol_data_list = []
         num_sc = input.num_sc()
         datos = dia_promedio_dataframe.get()
+        
+        sol_data_list = []
+        
         for sc_id in range(1, num_sc + 1):
             df = datos.copy()
             # Crear datos base para cada sistema constructivo
@@ -82,20 +91,24 @@ def server(input, output, session):
             # Resolver para este sistema constructivo
             sol_data = eh.solveCS(sc, sol_data)
             
+            # Agregar Ta solo la primera vez
+            if sc_id == 1:
+                sol_data_list.append(sol_data[["Tn", "DeltaTn", "Ta", "Ig", "Ib", "Id"]])
+            
             # Agregar identificador
-            sub_sol = sol_data[["Ta","Is","Tsa","Ti"]].add_suffix(f"_{sc_id}")
+            sub_sol = sol_data[["Is","Tsa","Ti"]].add_suffix(f"_{sc_id}")
             sol_data_list.append(sub_sol)
             
         # Combinar todos los resultados
         if sol_data_list:
-            resultado = pd.concat(sol_data_list)
+            resultado = pd.concat(sol_data_list,axis=1)
             soluciones_dataframe.set(resultado)
 
     
     def sistemaConstructivo(sc_id):
         return [
             (input[f"material_capa_{sc_id}_{i}"](), input[f"ancho_capa_{sc_id}_{i}"]())
-            for i in capas_activas().get(sc_id, [])
+            for i in capas_activas().get(sc_id)
         ]
         
     
@@ -126,10 +139,10 @@ def server(input, output, session):
     @reactive.Effect
     def _():
         # Buscar qué botón de agregar capa fue presionado
-        for sc_id in range(1, input.num_sc() + 1):
+        for sc_id in capas_activas():
             btn_id = f"add_capa_{sc_id}"
             if btn_id in input and input[btn_id]() > 0:
-                current_capas = capas_activas().get(sc_id, [1])
+                current_capas = capas_activas().get(sc_id)
                 nueva_capa = max(current_capas) + 1 if current_capas else 1
                 
                 new_dict = capas_activas().copy()
@@ -193,7 +206,7 @@ def server(input, output, session):
     def help_ui_capas(sc_id):
         """Función helper para generar la UI de capas para un SC específico"""
         panels = []
-        for i in capas_activas().get(sc_id, [1]):
+        for i in capas_activas().get(sc_id,[1]):
             panels.append(
                 ui.accordion_panel(
                     f"Capa {i}",
@@ -253,33 +266,72 @@ def server(input, output, session):
     @render_widget
     def sol_plot():
         sol_data = soluciones_dataframe.get()
-        if sol_data.empty:
-            dia_data = dia_promedio_dataframe.get()
-            if dia_data.empty: return None
-            
+        dia_data = dia_promedio_dataframe.get()
+        
+        if dia_data.empty: return None
+        
+        if sol_data.empty:        
             # Gráfica de día promedio
             display_data = dia_data.copy()[::60]
             solucion_plot = px.scatter(
                 data_frame=display_data,
                 x=display_data.index,
                 y=["Ta"]
-            ).add_hrect(
-                y0=dia_data["Tn"].mean()-dia_data["DeltaTn"].mean(),
-                y1=dia_data["Tn"].mean()+dia_data["DeltaTn"].mean(),
-                fillcolor="lime",
-                opacity=0.3,
-                line_width=0
             )
             
         else :
             display_data=sol_data.copy()
+            columnas = []
+            for i in display_data.columns[1:]:
+                if i.startswith("T"):
+                    columnas.append(i)
             solucion_plot = px.scatter(
                 data_frame=display_data,
                 x=display_data.index,
-                y=display_data.columns
+                y=columnas
             )
         
+        # Franja horizontal
+        solucion_plot.add_hrect(
+            y0=display_data["Tn"].mean()-display_data["DeltaTn"].mean(),
+            y1=display_data["Tn"].mean()+display_data["DeltaTn"].mean(),
+            fillcolor="lime",
+            opacity=0.3,
+            line_width=0
+            )
         return solucion_plot
+    
+    @render_widget
+    def irr_plot():
+        sol_data = soluciones_dataframe.get()
+        dia_data = dia_promedio_dataframe.get()
+        
+        if dia_data.empty: return None
+        
+        if sol_data.empty:        
+            # Gráfica solo con día promedio
+            display_data = dia_data.copy()[::60]
+            solucion_plot = px.scatter(
+                data_frame=display_data,
+                x=display_data.index,
+                y=["Ig","Ib","Id"]
+            )
+            
+        else :
+            display_data=sol_data.copy()
+            
+            columnas = []
+            for i in display_data.columns[1:]:
+                if i.startswith("I"):
+                    columnas.append(i)
+            solucion_plot = px.scatter(
+                data_frame=display_data,
+                x=display_data.index,
+                y=columnas
+            )
+            
+        return solucion_plot
+        
             
         
 
